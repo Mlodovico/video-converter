08434ba94825dfacd2fd03bcfbf78edc27007eeb
Murilo Lodovico <37813008+Mlodovico@users.noreply.github.com>
2023-06-15 00:21:23 -0300

    feat: trying to fetch style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/src/styles/Home.module.css";
 import { useState } from "react";
@@ -26,24 +25,26 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={`${styles.main}`}>
         <div className={styles.description}>
-          <p className="text-2xl font-bold text-zinc-500">
-            Coloque o seu video logo a baixo!
-          </p>
-          <div className="grid grid-cols-3 justify-items-stretch">
-            <input
-              onChange={(e) => handleLink(e.target.value)}
-              type="text"
-              className="border-2 rounded-md p-3 border-gray-300 bg-slate-300 md:w-2/3"
-              placeholder="Coloque o seu video aqui"
-            />
-            <button
-              onClick={handleWebAssembly}
-              className="h-full p-3 bg-blue-400 rounded-md md:w-1/3"
-            >
-              <AiOutlineSend />
-            </button>
+          <div className="justify-itens-center">
+            <p className="text-2xl font-bold text-zinc-500 justify-self-center">
+              Put the video link right here!
+            </p>
+            <div className="justify-self-center">
+              <input
+                onChange={(e) => handleLink(e.target.value)}
+                type="text"
+                className="border-2 rounded-md p-3 border-gray-300 bg-slate-300 w-10"
+                placeholder="Coloque o seu video aqui"
+              />
+              <button
+                onClick={handleWebAssembly}
+                className="h-full p-3 bg-blue-400 rounded-md"
+              >
+                <AiOutlineSend />
+              </button>
+            </div>
           </div>
         </div>
       </main>
