fb47cc6378f6d621e31409df52ab9a7a6b2012c3
Murilo Lodovico <37813008+Mlodovico@users.noreply.github.com>
2023-06-16 00:02:48 -0300

    feat: starting C project

---
+++ src/WASM/videoConverter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <libavformat/avformat.h>
+
+int main()
+{
+    av.register_all();
+    const chat *video_url = "https://www.youtube.com/watch?v=D1Y9Y4D2drk";
+    printf("Video Converter\n");
+}
